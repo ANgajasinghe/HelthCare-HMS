e951255a54e486cacdf95fcb457b8007f1e466ab
angajasinghe@outlook.com <angajasinghe@gmail.com>
2020-03-26 17:38:19 +0530

    add room service

---
+++ RMService/src/main/java/model/Rooms.java
@@ -0,0 +1,64 @@
+
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Rooms {
+
+	private Connection connect()
+	{
+	Connection con = null;
+	try
+	{
+	Class.forName("com.mysql.jdbc.Driver");
+	//Provide the correct details: DBServer/DBName, username, password
+	con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
+	}
+	catch (Exception e)
+	{e.printStackTrace();}
+	return con;
+	}
+	
+	public String readRooms()
+	{
+	String output = "";
+	try
+	{
+	Connection con = connect();
+	if (con == null)
+	{return "Error while connecting to the database for reading."; }
+	// Prepare the html table to be displayed
+	output = "<table border=\"1\"><tr><th>room</th><th>room NO</th><th>Item Type</th></tr>";
+	String query = "select * from rooms";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	// iterate through the rows in the result set
+	while (rs.next())
+	{
+	//String ID = Integer.toString(rs.getInt("ID"));
+	String roomNo = rs.getString("RoomNo");
+	String roomType = rs.getString("RoomType");
+	
+	// Add into the html table
+	output += "<tr><td>" + roomNo + "</td>";
+	output += "<td>" + roomType + "</td>";
+	
+	
+	
+	}
+	con.close();
+	// Complete the html table
+	output += "</table>";
+	}
+	catch (Exception e)
+	{
+		output = "Error while reading the items.";
+		System.err.println(e.getMessage());
+	}
+		return output;
+	}
+	
+}
